d9f533ed339a8bc7eda1869de3c82a56a1b069d2
Thomas Shockman <tom.shockman@gmail.com>
2019-02-24 19:34:24 -0600

    Enable cors

--- server/server.js
+++ server/server.js
@@ -1,8 +1,13 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const cors = require('cors');
+
 const {PORT} = require('./config');
 
 const app = express();
+
+// enable cors
+app.use(cors());
 
 // parse json
 app.use(bodyParser.json());
