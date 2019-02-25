56336bba449e04d064648b64a989e999ffb647dc
Thomas Shockman <tom.shockman@gmail.com>
2019-02-24 22:27:40 -0600

    Do not allow storage of duplicate bookmarks

--- server/services/bookmarks.js
+++ server/services/bookmarks.js
@@ -10,7 +10,12 @@ module.exports = app => {
   const bookmarkProject = (req, res) => {
     const repo = req.body;
     if (repo) {
-      bookmarks.push(repo);
+      const currentIndex = bookmarks.findIndex(r => r.id === repo.id);
+      if (currentIndex === -1) {
+        bookmarks.push(repo);
+      } else {
+        bookmarks[currentIndex] = repo;
+      }
       res.sendStatus(200);
     } else {
       res.sendStatus(400);
